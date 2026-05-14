54d98a33505665e3ff6091b7422cf09fb560b46a
Lucas Goes <lucas--goes@hotmail.com>
2026-05-14 16:49:31 -0300

    fix(api): type root handler params explicitly

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { type RequestHandler } from "express";
+import express, { type Request, type RequestHandler, type Response } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
 import router from "./routes/index.js";
@@ -40,7 +40,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (_req, res) => {
+app.get("/", (_req: Request, res: Response) => {
   res.json({ service: "api-server", status: "ok" });
 });
 
